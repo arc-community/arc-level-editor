e5e9d063135a6dc863f53fee66f37ff7e9ff5881
Yannic Kilcher <yk@users.noreply.github.com>
2022-07-29 21:50:03 +0200

    first working version

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import Head from 'next/head'
 
-const EXAMPLE = {"test": [{"input": [[7, 0, 7], [7, 0, 7], [7, 7, 0]], "output": [[7, 0, 7, 0, 0, 0, 7, 0, 7], [7, 0, 7, 0, 0, 0, 7, 0, 7], [7, 7, 0, 0, 0, 0, 7, 7, 0], [7, 0, 7, 0, 0, 0, 7, 0, 7], [7, 0, 7, 0, 0, 0, 7, 0, 7], [7, 7, 0, 0, 0, 0, 7, 7, 0], [7, 0, 7, 7, 0, 7, 0, 0, 0], [7, 0, 7, 7, 0, 7, 0, 0, 0], [7, 7, 0, 7, 7, 0, 0, 0, 0]]}], "train": [{"input": [[0, 7, 7], [7, 7, 7], [0, 7, 7]], "output": [[0, 0, 0, 0, 7, 7, 0, 7, 7], [0, 0, 0, 7, 7, 7, 7, 7, 7], [0, 0, 0, 0, 7, 7, 0, 7, 7], [0, 7, 7, 0, 7, 7, 0, 7, 7], [7, 7, 7, 7, 7, 7, 7, 7, 7], [0, 7, 7, 0, 7, 7, 0, 7, 7], [0, 0, 0, 0, 7, 7, 0, 7, 7], [0, 0, 0, 7, 7, 7, 7, 7, 7], [0, 0, 0, 0, 7, 7, 0, 7, 7]]}, {"input": [[4, 0, 4], [0, 0, 0], [0, 4, 0]], "output": [[4, 0, 4, 0, 0, 0, 4, 0, 4], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 4, 0, 0, 0, 0, 0, 4, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 4, 0, 4, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 4, 0, 0, 0, 0]]}, {"input": [[0, 0, 0], [0, 0, 2], [2, 0, 2]], "output": [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 2], [0, 0, 0, 0, 0, 0, 2, 0, 2], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 2, 0, 0, 0, 0, 0, 2], [2, 0, 2, 0, 0, 0, 2, 0, 2]]}, {"input": [[6, 6, 0], [6, 0, 0], [0, 6, 6]], "output": [[6, 6, 0, 6, 6, 0, 0, 0, 0], [6, 0, 0, 6, 0, 0, 0, 0, 0], [0, 6, 6, 0, 6, 6, 0, 0, 0], [6, 6, 0, 0, 0, 0, 0, 0, 0], [6, 0, 0, 0, 0, 0, 0, 0, 0], [0, 6, 6, 0, 0, 0, 0, 0, 0], [0, 0, 0, 6, 6, 0, 6, 6, 0], [0, 0, 0, 6, 0, 0, 6, 0, 0], [0, 0, 0, 0, 6, 6, 0, 6, 6]]}, {"input": [[2, 2, 2], [0, 0, 0], [0, 2, 2]], "output": [[2, 2, 2, 2, 2, 2, 2, 2, 2], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 2, 2, 0, 2, 2, 0, 2, 2], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 2, 2, 2, 2, 2, 2], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 2, 2, 0, 2, 2]]}]};
+const EXAMPLE_BOARD = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
+const EXAMPLE = {
+  train: [
+    {input: EXAMPLE_BOARD, output: EXAMPLE_BOARD},
+  ],
+  test: [
+    {input: EXAMPLE_BOARD, output: EXAMPLE_BOARD},
+  ],
+}
 
 const COLOR_MAP = {
   0: "bg-black",
@@ -16,63 +29,177 @@ const COLOR_MAP = {
   9: "bg-pink-600",
 };
 
-function BoardSection({children, title, bg_color="bg-white"}){
-  return <div className={`${bg_color}`}>
+function BoardSection({children, title, bg_color="bg-white", addPair}){
+  return <div className={`${bg_color} my-8 p-6`}>
     <h2 className={`text-3xl font-bold text-center`}>{title}</h2>
     {children}
+    <div className="flex flex-row justify-center">
+      <Button onClick={() => addPair()}>Add Pair</Button>
+    </div>
   </div>;
 }
 
-function Board({data, setValue}){
+function Board({data, setValue, setDimensions}){
   const num_rows = data.length;
   const num_cols = data[0].length;
   return (<div>
     <div>
-      Rows: {num_rows}
+      Rows: <input min="1" max="30" type="number" value={num_rows} onChange={ev => setDimensions({rows: ev.target.value, cols: num_cols})}/>
       <br/>
-      Cols: {num_cols}
+      Cols: <input min="1" max="30" type="number" value={num_cols} onChange={ev => setDimensions({rows: num_rows, cols: ev.target.value})}/>
     </div>
     <div className="mx-8 flex flex-col">
       {data.map((row, row_idx) => <div key={row_idx} className="flex flex-row">
         {row.map((val, col_idx) => 
-          <div key={col_idx} className={`h-8 w-8 ${COLOR_MAP[val]} text-center mr-px mb-px`} onClick={() => setValue({
+          <div key={col_idx} className={`h-8 w-8 ${COLOR_MAP[val]} text-center mr-px mb-px select-none`} onClick={() => setValue({
             i: row_idx,
             j: col_idx,
-            val: (val + 1) % 10,
-          })}>{val}</div>
+          })}>&nbsp;</div>
         )}
       </div>)}
     </div>
   </div>);
 }
 
-function BoardPair({input, output, setValue}){
+function Button({children, ...args}){
+  return <button className="border rounded p-2 border-1" {...args}>{children}</button>;
+}
+
+function addParams(func, params){
+  return ({...args}) => func({...args, ...params});
+}
+
+function BoardPair({input, output, setValue, setDimensions, deletePair}){
+  function forInput(func){
+    return addParams(func, {board_name: "input"});
+  }
+  function forOutput(func){
+    return addParams(func, {board_name: "output"});
+  }
   return <div className="flex flex-row items-center my-8">
-    <Board data={input} setValue={({...args}) => setValue({...args, board: "input"})}/>
-    <Board data={output} setValue={({...args}) => setValue({...args, board: "output"})}/>
+    <Board data={input} setValue={forInput(setValue)} setDimensions={forInput(setDimensions)}/>
+    <Board data={output} setValue={forOutput(setValue)} setDimensions={forOutput(setDimensions)}/>
+    <div>
+    <Button onClick={() => deletePair()}>Delete Pair</Button>
+    </div>
   </div>;
+}
+
+function addRow(board){
+  board.push(board[board.length-1]);
+}
+
+function removeRow(board){
+  board.splice(board.length-1);
+}
+
+function addColumn(board){
+  board.forEach(col => col.push(col[col.length-1]));
+}
+
+function removeColumn(board){
+  board.forEach(col => col.splice(col.length-1));
+}
+
+function deepCopy(obj){
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function ColorSelector({color, setColor}){
+  return (<div className="fixed top-0 w-screen h-16 flex flex-row">
+    {Object.keys(COLOR_MAP).map(c => <div 
+      key={c}
+      className={`w-full h-full select-none text-center ${COLOR_MAP[c]} ${c == color ? "border-4 border-white" : ""}`}
+      onClick={() => setColor(c)}
+    >
+      &nbsp;
+    </div>)}
+  </div>);
 }
 
 export default function Home() {
   const [riddle, setRiddle] = React.useState(EXAMPLE);
+  const [color, setColor] = React.useState(0);
 
-  function setValue({set_name, pair_idx, board, i, j, val}){
+  function setValue({set_name, pair_idx, board_name, i, j}){
     setRiddle(riddle => {
-      const new_riddle = JSON.parse(JSON.stringify(riddle));
-      new_riddle[set_name][pair_idx][board][i][j] = val;
+      const new_riddle = deepCopy(riddle);
+      new_riddle[set_name][pair_idx][board_name][i][j] = color;
       return new_riddle;
     })
+  }
+
+  function setDimensions({set_name, pair_idx, board_name, rows, cols}){
+    if(rows < 1 || cols < 1){
+      return;
+    }
+    setRiddle(riddle => {
+      const new_riddle = deepCopy(riddle);
+      const board = new_riddle[set_name][pair_idx][board_name];
+      while(board.length < rows){
+        addRow(board);
+      }
+      while(board.length > rows){
+        removeRow(board);
+      }
+      while(board[0].length < cols){
+        addColumn(board);
+      }
+      while(board[0].length > cols){
+        removeColumn(board);
+      }
+      return new_riddle;
+    })
+  }
+
+  function addPair({set_name}){
+    setRiddle(riddle => {
+      const new_riddle = deepCopy(riddle);
+      const rset = new_riddle[set_name];
+      rset.push(deepCopy(rset[rset.length-1]));
+      return new_riddle;
+    });
+  }
+
+  function deletePair({set_name, pair_idx}){
+    setRiddle(riddle => {
+      const new_riddle = deepCopy(riddle);
+      const rset = new_riddle[set_name];
+      if(rset.length > 1){
+        rset.splice(pair_idx, 1);
+      }
+      return new_riddle;
+    });
+  }
+
+  function forTrain(func, pair_idx){
+    return addParams(func, {pair_idx, set_name: 'train'});
+  }
+
+  function forTest(func, pair_idx){
+    return addParams(func, {pair_idx, set_name: 'test'});
   }
 
   return (
     <>
       <Head><title>ARC Level Editor</title></Head>
-      <div className="w-screen flex flex-col items-center">
-        <BoardSection title="Train">
-          {riddle.train.map((board_pair, idx) => <BoardPair key={idx} {...board_pair} setValue={({...args})=>setValue({...args, set_name: 'train', pair_idx:idx})}/>)}
+      <ColorSelector color={color} setColor={setColor}/>
+      <div className="w-screen flex flex-col items-center mt-8">
+        <BoardSection title="Train" addPair={forTrain(addPair)}>
+          {riddle.train.map((board_pair, pair_idx) => <BoardPair 
+            key={pair_idx} {...board_pair} 
+            setValue={forTrain(setValue, pair_idx)}
+            setDimensions={forTrain(setDimensions, pair_idx)}
+            deletePair={forTrain(deletePair, pair_idx)}
+            />)}
         </BoardSection>
-        <BoardSection title="Test" bg_color="bg-slate-300">
-          {riddle.test.map((board_pair, idx) => <BoardPair key={idx} {...board_pair} setValue={({...args})=>setValue({...args, set_name: 'test', pair_idx:idx})}/>)}
+        <BoardSection title="Test" bg_color="bg-slate-300" addPair={forTest(addPair)}>
+          {riddle.test.map((board_pair, pair_idx) => <BoardPair 
+            key={pair_idx} {...board_pair} 
+            setValue={forTest(setValue, pair_idx)}
+            setDimensions={forTest(setDimensions, pair_idx)}
+            deletePair={forTest(deletePair, pair_idx)}
+            />)}
         </BoardSection>
       </div>
       </>
